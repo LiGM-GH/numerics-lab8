1685848ceadd003add14994ee64564155f9f4d54
LiGM-GH <lipkin.grisha@mail.ru>
2023-12-11 15:47:07 +0300

    Something like this

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # README.md
-
+\
 ## Task 8.1
 
 +---+---+-----+---+----------+------+-----+
@@ -17,51 +17,23 @@ Find solituon for
 
 My problem:
   _
- /  -u'' - u' = -2x + 3, x ∈ (2, 4)
--|  u(2) = -12
- \_ u(4) = -10
+ /  -u'' - u' = 4x + 2, x ∈ (0, 3)
+-|  u(2) = 2
+ \_ u(4) = 0
 ```
 ### Analytical solution:
 ```math
-Let's integrate the whole equation:
--u' - u = -x² + 3x
--λ - 1 = 0; λ = -1;
-u = C * e^(-x)
-u' = C' * e^(-x) - C * e(-x)
--C' * e^(-x) = -x² + 3x
--C =
-= ∫ {3 * x * e^x} dx + ∫ {-e^x * x²} dx =
-= |
-  | u = x
-  | dv = e^x dx
-  | v = e^x
-  | du = dx
-
-= ∫ {-e^x * x²} dx + 3 * (x * e^x - ∫ {e^x} dx) =
-= |
-  | u = x^2
-  | dv = e^x dx
-  | v = e^x
-  | du = 2xdx
-= -(x² * e^x - 2 * ∫ {e^x * x} dx) + 3 * (x - 1) * e^x =
-=              |
-               | u = x
-               | dv = e^x dx
-               | v = e^x
-               | du = dx
-= -(x² * e^x - 2 * (x*e^x - ∫ {e^x} dx)) + 3 * (x - 1) * e^x =
-= -x² * e^x + 2 * x * e^x - 2 * e^x + 3 * (x - 1) * e^x =
-= -x² * e^x + 5 * x * e^x - 5 * e^x
-
-Then
-C =
-= -x² * e^x + 5 * x * e^x - 5 * e^x
-
-Then
-u = x² - 5 * x + 5
-u' = 2x - 5
-u'' = 2
--2 - 2 x + 5 = -2x + 3
+Ackshually, the solution is:
+u = 2 * e^3 / (e^3 - 1) * e^(-x) - 2 / (e^3 - 1) - 2x² + 6x
 ```
 
 ## Looking for approximate answer with accuracy of ε = 0.001
+
+What if we look at this:
+```math
+-u'' - u' = -2x + 3, x ∈ (2, 4)
+```
+as
+```math
+-(u'*e^x)' = (-2x + 3)*e^x, where k(x) = e^x
+```
--- src/pt1.rs
+++ src/pt1.rs
@@ -1,4 +1,7 @@
-use error_stack::Result;
+use error_stack::{Result, ResultExt};
+use ndarray::{Array1, Array2, Dim, ShapeBuilder};
+use ndarray_linalg::Solve;
+use std::f64::consts::E;
 
 use crate::error::SomeError;
 
@@ -7,5 +10,103 @@ mod consts {
 }
 
 pub fn main() -> Result<(), SomeError> {
-    unimplemented!();
+    let start = 0.0;
+    let end = 3.0;
+
+    let values = make_values(start, end, (10, 10));
+
+    println!("{}", values);
+
+    let rhs = make_rhs(start, end, 10);
+
+    println!("{}", rhs);
+
+    let answer: Array1<_> = values
+        .solve_into(rhs)
+        .map_err(|err| error_stack::report!(err))
+        .change_context::<SomeError>(SomeError)?;
+
+    println!("{answer}");
+
+    let real_y = make_real_y(start, end, 10);
+
+    println!("{}", real_y);
+
+    Ok(())
+}
+
+fn f(x: f64) -> f64 {
+    4.0 * x - 2.0
+}
+
+fn k(x: f64) -> f64 {
+    f64::exp(x)
+}
+
+fn real_y(x: f64) -> f64 {
+    2.0 * E.powi(3) / (E.powi(3) - 1.0) * E.powf(-x) - 2.0 / (E.powi(3) - 1.0) - 2.0 * x.powi(2)
+        + 6.0 * x
+}
+
+fn make_values(
+    start: f64,
+    end: f64,
+    shape: impl ShapeBuilder<Dim = Dim<[usize; 2]>>,
+) -> Array2<f64> {
+    let mut values = Array2::default(shape);
+    let h = (end - start) / (values.shape()[0] as f64 - 1.0);
+
+    let mut i = 0;
+    *values.get_mut((0, 0)).unwrap() = 1.0;
+    i += 1;
+
+    let len = values.shape()[0] - 2;
+
+    for mut row in values.rows_mut().into_iter().skip(1).take(len) {
+        *row.get_mut(i - 1).unwrap() = -k(start + (i as f64 - 0.5) * h);
+        *row.get_mut(i).unwrap() =
+            k(start + (i as f64 - 0.5) * h) + k(start + (i as f64 + 0.5) * h);
+        *row.get_mut(i + 1).unwrap() = -k(start + (i as f64 + 0.5) * h);
+        i += 1;
+    }
+
+    *values.get_mut((len + 1, len + 1)).unwrap() = 1.0;
+    values
+}
+
+fn make_rhs(start: f64, end: f64, shape: impl ShapeBuilder<Dim = Dim<[usize; 1]>>) -> Array1<f64> {
+    let mut rhs = Array1::default(shape);
+    let h = (end - start) / (rhs.shape()[0] as f64 - 1.0);
+
+    let mut i = 0;
+
+    *rhs.get_mut(0).unwrap() = 2.0;
+
+    i += 1;
+
+    let len = rhs.shape()[0];
+    for value in rhs.iter_mut().skip(1) {
+        *value = h.powi(2) * f(start + h * i as f64);
+
+        i += 1;
+    }
+
+    *rhs.get_mut(len - 1).unwrap() = 0.0;
+
+    rhs
+}
+
+fn make_real_y(
+    start: f64,
+    end: f64,
+    shape: impl ShapeBuilder<Dim = Dim<[usize; 1]>>,
+) -> Array1<f64> {
+    let mut real_y_values = Array1::default(shape);
+    let h = (end - start) / (real_y_values.shape()[0] as f64 - 1.0);
+
+    for (i, value) in real_y_values.iter_mut().enumerate() {
+        *value = real_y(start + h * i as f64);
+    }
+
+    real_y_values
 }
